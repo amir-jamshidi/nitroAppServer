1f59181c910eef98705fc6d8c0a59fa59bd4957a
Amir Jamshidi <aabbccddee9898@gmail.com>
2023-11-21 01:21:21 +0330

    Feat : Fix GetMe Route

--- Routers/Auth.js
+++ Routers/Auth.js
@@ -1,8 +1,10 @@
 const express = require('express');
-const { register, confirmUser, login } = require('../Controllers/Auth');
+const { register, confirmUser, login, getMe } = require('../Controllers/Auth');
+const isLoginUserMiddle = require('../Middlewares/isLoginUser');
 const router = express.Router();
 
 router.route('/register').post(register);
 router.route('/confirm').post(confirmUser);
 router.route('/login').post(login);
+router.route('/me').get(isLoginUserMiddle, getMe);
 module.exports = router;
